4f242257c667d7104279a7a2192a904c8bea4045
Sheinyy <g.backes@t-online.de>
2023-01-23 17:46:29 +0100

    Jeopardy auf Seite gemacht

---
+++ js/config.js
@@ -0,0 +1,155 @@
+const config = {
+
+    Fragen: {
+        q11 :{
+            "Fragentyp" : "",
+            "Frage" : "Diese Firma ist Publisher von Fortnite und sie haben ihren eigenen Store um Steam Konkurrenz zu machen",
+            "Antwort" : "Epic Games",
+        },
+        q12 :{
+            "Fragentyp" : "",
+            "Frage" : 'Dieser Spielehersteller ist der Besitzer von Steam und Hersteller von CS:GO, TF2 und Dota',
+            "Antwort" : "Valve",
+        },
+        q13 :{
+            "Fragentyp" : "Text",
+            "Frage" : "Diese Firma kaufte letztes Jahr Blizzard/Activision und ist der Hersteller der X-Box",
+            "Antwort" : "Microsoft",
+        },
+        q14 :{
+            "Fragentyp" : "",
+            "Frage" : 'Wie viele Spiele mit der Wortkombination "Super Mario" im Titel wurden für die Nintendo Switch released?',
+            "Antwort" : "5 : Odyssey, Party, 3D Allstars, 3D World, Mario Maker",
+        },
+        q15 :{
+            "Fragentyp" : "",
+            "Frage" : 'Dieser Entwickler ist verantwortlich für die erfolgreiche Spieleserie "God of War"',
+            "Antwort" : "Santa Monica Studio",
+        },
+        q16 :{
+            "Fragentyp" : "",
+            "Frage" : "",
+            "Antwort" : "",
+        },
+        q21 :{
+            "Fragentyp" : "",
+            "Frage" : "Ein Siedegebäck aus süßem Hefeteig und einer Füllung aus Konfitüre aus der Hauptstadt Deutschlands",
+            "Antwort" : "Berliner",
+        },
+        q22 :{
+            "Fragentyp" : "",
+            "Frage" : "Das beliebteste Fast Food der Welt, aus der größten Hansestadt Deutschlands",
+            "Antwort" : "Hamburger",
+        },
+        q23 :{
+            "Fragentyp" : "",
+            "Frage" : "Eine Torte aus Sandmasse mit Sauerkirschen, Buttercreme und Kakao, entsanden an dem zweitgrößten Fluss Europas",
+            "Antwort" : "Donauwelle",
+        },
+        q24 :{
+            "Fragentyp" : "",
+            "Frage" : "Diese gekochten Fleischklöße, in weißer Sauche mit Kapern, kommen aus der Hauptstadt Preußens",
+            "Antwort" : "Königsberger Klopse",
+        },
+        q25 :{
+            "Fragentyp" : "",
+            "Frage" : "Dieser französische Weißschimmelkäse ist in Deutschland beliebt, obwohl viele ihn nicht richtig Schreiben können",
+            "Antwort" : "Camembert",
+        },
+        q26 :{
+            "Fragentyp" : "",
+            "Frage" : "",
+            "Antwort" : "",
+        },
+        q31 :{
+            "Fragentyp" : "",
+            "Frage" : "In Deutschland wurde diese Art von Content durch Unge etabliert und ist seitdem Bestandteil von den Streams vieler Influencer",
+            "Antwort" : "Reactions",
+        },
+        q32 :{
+            "Fragentyp" : "",
+            "Frage" : "Anfang 2021 war es Trend auf Twitch diese Sammelkarten zu öffnen",
+            "Antwort" : "Pokemon",
+        },
+        q33 :{
+            "Fragentyp" : "",
+            "Frage" : "Diese Zwei Spiele sind nicht nur an der Spitze der Twitch Kategorien, sondern auch die meistverkauftesten Spiele allerzeiten",
+            "Antwort" : "GTA V und Minecraft",
+        },
+        q34 :{
+            "Fragentyp" : "",
+            "Frage" : "Dieser amerikanische Streamer pionierte Spenden anzunehmen fürs Streamen",
+            "Antwort" : "Reckful",
+        },
+        q35 :{
+            "Fragentyp" : "",
+            "Frage" : "Was bedeutet das Präfix Meta?",
+            "Antwort" : "Auf einer höheren Stufe, Ebene befinden",
+        },
+        q36 :{
+            "Fragentyp" : "",
+            "Frage" : "",
+            "Antwort" : "",
+        },
+        q41 :{
+            "Fragentyp" : "Bild",
+            "Frage" : "Wie viele Streifen hat die Belgische Flagge?",
+            "Antwort" : "3",
+        },
+        q42 :{
+            "Fragentyp" : "",
+            "Frage" : "Aus welchen Farben besteht die französische Flagge?",
+            "Antwort" : "Rot Weiß Blau",
+        },
+        q43 :{
+            "Fragentyp" : "",
+            "Frage" : "Die Polnische Flagge besteht aus zwei Farben. Welche ist die obere der beiden Balken?",
+            "Antwort" : "Weiß",
+        },
+        q44 :{
+            "Fragentyp" : "",
+            "Frage" : "Die Anzahl der Sterne, der Europäischen Flagge",
+            "Antwort" : "12",
+        },
+        q45 :{
+            "Fragentyp" : "",
+            "Frage" : "Welches Land hat die einzige Flagge der Welt mit mehr als Vier Ecken?",
+            "Antwort" : "Nepal",
+        },
+        q46 :{
+            "Fragentyp" : "",
+            "Frage" : "",
+            "Antwort" : "",
+        },
+        q51 :{
+            "Fragentyp" : "",
+            "Frage" : "Ein deutscher Influencer der Hauptsächlich Reactions macht. Immer in seinem Format um 20:00 Uhr",
+            "Antwort" : "Unge",
+        },
+        q52 :{
+            "Fragentyp" : "",
+            "Frage" : "Dank dieser kleinen Bilder im Chat, reagieren Zuschauer auf Twitch in ihrer ganz eigenen Sprache, auf das was ein Streamer macht",
+            "Antwort" : "Emotes",
+        },
+        q53 :{
+            "Fragentyp" : "",
+            "Frage" : "Dieses Dateiformat wird oft benutzt für kurze Videos die sich wiederholen um auf etwas zu reagieren",
+            "Antwort" : "GIF",
+        },
+        q54 :{
+            "Fragentyp" : "",
+            "Frage" : "Diese Art von Gas ist unreaktiv und einatomig",
+            "Antwort" : "Edelgas",
+        },
+        q55 :{
+            "Fragentyp" : "",
+            "Frage" : "Durch diese Art von Reaktion bildet sich Rost",
+            "Antwort" : "Oxidation",
+        },
+        q56 :{
+            "Fragentyp" : "",
+            "Frage" : "",
+            "Antwort" : "",
+        }
+    }
+}
