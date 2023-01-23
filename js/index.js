2b5e659c61182b25bde48492ac5897948bbd2526
Sheinyy <g.backes@t-online.de>
2023-01-23 17:24:49 +0100

    Online Test

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function Testfun(){
+    let text="es funktioniert vllt";
+    let Kasten = document.getElementById("Test");
+    let Textfeld = document.createTextNode(text);
+    Kasten.appendChild(Textfeld);
+}
