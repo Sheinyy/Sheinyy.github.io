8fe3ab30fe2718ce3b3dd968a468826a3fa9b64a
Sheinyy <g.backes@t-online.de>
2023-01-23 18:47:42 +0100

    Normale und Doppelte Punkte
    jetzt aber richtig

---
+++ js/gamemode.js
@@ -0,0 +1,257 @@
+function selectGamemode(gamemode){
+    if(gamemode=='Normal'){
+        let e1 = document.createTextNode("100");
+        let e2 = document.createTextNode("100");
+        let e3 = document.createTextNode("100");
+        let e4 = document.createTextNode("100");
+        let e5 = document.createTextNode("100");
+        let z1 = document.createTextNode("200");
+        let z2 = document.createTextNode("200");
+        let z3 = document.createTextNode("200");
+        let z4 = document.createTextNode("200");
+        let z5 = document.createTextNode("200");
+        let d1 = document.createTextNode("300");
+        let d2 = document.createTextNode("300");
+        let d3 = document.createTextNode("300");
+        let d4 = document.createTextNode("300");
+        let d5 = document.createTextNode("300");
+        let v1 = document.createTextNode("400");
+        let v2 = document.createTextNode("400");
+        let v3 = document.createTextNode("400");
+        let v4 = document.createTextNode("400");
+        let v5 = document.createTextNode("400");
+        let f1 = document.createTextNode("500");
+        let f2 = document.createTextNode("500");
+        let f3 = document.createTextNode("500");
+        let f4 = document.createTextNode("500");
+        let f5 = document.createTextNode("500");
+        let text11 = document.createElement("p");
+        text11.appendChild(e1);
+        text11.style.margin = "7px"
+        document.getElementById("Box11").appendChild(text11);
+        let text21 = document.createElement("p");
+        text21.appendChild(e2);
+        text21.style.margin = "7px"
+        document.getElementById("Box21").appendChild(text21);
+        let text31 = document.createElement("p");
+        text31.appendChild(e3);
+        text31.style.margin = "7px"
+        document.getElementById("Box31").appendChild(text31);
+        let text41 = document.createElement("p");
+        text41.appendChild(e4);
+        text41.style.margin = "7px"
+        document.getElementById("Box41").appendChild(text41);
+        let text51 = document.createElement("p");
+        text51.appendChild(e5);
+        text51.style.margin = "7px"
+        document.getElementById("Box51").appendChild(text51);
+        let text12 = document.createElement("p");
+        text12.appendChild(z1);
+        text12.style.margin = "7px"
+        document.getElementById("Box12").appendChild(text12);
+        let text22 = document.createElement("p");
+        text22.appendChild(z2);
+        text22.style.margin = "7px"
+        document.getElementById("Box22").appendChild(text22);
+        let text32 = document.createElement("p");
+        text32.appendChild(z3);
+        text32.style.margin = "7px"
+        document.getElementById("Box32").appendChild(text32);
+        let text42 = document.createElement("p");
+        text42.appendChild(z4);
+        text42.style.margin = "7px"
+        document.getElementById("Box42").appendChild(text42);
+        let text52 = document.createElement("p");
+        text52.appendChild(z5);
+        text52.style.margin = "7px"
+        document.getElementById("Box52").appendChild(text52);
+        let text13 = document.createElement("p");
+        text13.appendChild(d1);
+        text13.style.margin = "7px"
+        document.getElementById("Box13").appendChild(text13);
+        let text23 = document.createElement("p");
+        text23.appendChild(d2);
+        text23.style.margin = "7px"
+        document.getElementById("Box23").appendChild(text23);
+        let text33 = document.createElement("p");
+        text33.appendChild(d3);
+        text33.style.margin = "7px"
+        document.getElementById("Box33").appendChild(text33);
+        let text43 = document.createElement("p");
+        text43.appendChild(d4);
+        text43.style.margin = "7px"
+        document.getElementById("Box43").appendChild(text43);
+        let text53 = document.createElement("p");
+        text53.appendChild(d5);
+        text53.style.margin = "7px"
+        document.getElementById("Box53").appendChild(text53);
+        let text14 = document.createElement("p");
+        text14.appendChild(v1);
+        text14.style.margin = "7px"
+        document.getElementById("Box14").appendChild(text14);
+        let text24 = document.createElement("p");
+        text24.appendChild(v2);
+        text24.style.margin = "7px"
+        document.getElementById("Box24").appendChild(text24);
+        let text34 = document.createElement("p");
+        text34.appendChild(v3);
+        text34.style.margin = "7px"
+        document.getElementById("Box34").appendChild(text34);
+        let text44 = document.createElement("p");
+        text44.appendChild(v4);
+        text44.style.margin = "7px"
+        document.getElementById("Box44").appendChild(text44);
+        let text54 = document.createElement("p");
+        text54.appendChild(v5);
+        text54.style.margin = "7px"
+        document.getElementById("Box54").appendChild(text54);
+        let text15 = document.createElement("p");
+        text15.appendChild(f1);
+        text15.style.margin = "7px"
+        document.getElementById("Box15").appendChild(text15);
+        let text25 = document.createElement("p");
+        text25.appendChild(f2);
+        text25.style.margin = "7px"
+        document.getElementById("Box25").appendChild(text25);
+        let text35 = document.createElement("p");
+        text35.appendChild(f3);
+        text35.style.margin = "7px"
+        document.getElementById("Box35").appendChild(text35);
+        let text45 = document.createElement("p");
+        text45.appendChild(f4);
+        text45.style.margin = "7px"
+        document.getElementById("Box45").appendChild(text45);
+        let text55 = document.createElement("p");
+        text55.appendChild(f5);
+        text55.style.margin = "7px"
+        document.getElementById("Box55").appendChild(text55);
+    }else{
+        let e1 = document.createTextNode("200");
+        let e2 = document.createTextNode("200");
+        let e3 = document.createTextNode("200");
+        let e4 = document.createTextNode("200");
+        let e5 = document.createTextNode("200");
+        let z1 = document.createTextNode("400");
+        let z2 = document.createTextNode("400");
+        let z3 = document.createTextNode("400");
+        let z4 = document.createTextNode("400");
+        let z5 = document.createTextNode("400");
+        let d1 = document.createTextNode("600");
+        let d2 = document.createTextNode("600");
+        let d3 = document.createTextNode("600");
+        let d4 = document.createTextNode("600");
+        let d5 = document.createTextNode("600");
+        let v1 = document.createTextNode("800");
+        let v2 = document.createTextNode("800");
+        let v3 = document.createTextNode("800");
+        let v4 = document.createTextNode("800");
+        let v5 = document.createTextNode("800");
+        let f1 = document.createTextNode("1000");
+        let f2 = document.createTextNode("1000");
+        let f3 = document.createTextNode("1000");
+        let f4 = document.createTextNode("1000");
+        let f5 = document.createTextNode("1000");
+        let text11 = document.createElement("p");
+        text11.appendChild(e1);
+        text11.style.margin = "7px"
+        document.getElementById("Box11").appendChild(text11);
+        let text21 = document.createElement("p");
+        text21.appendChild(e2);
+        text21.style.margin = "7px"
+        document.getElementById("Box21").appendChild(text21);
+        let text31 = document.createElement("p");
+        text31.appendChild(e3);
+        text31.style.margin = "7px"
+        document.getElementById("Box31").appendChild(text31);
+        let text41 = document.createElement("p");
+        text41.appendChild(e4);
+        text41.style.margin = "7px"
+        document.getElementById("Box41").appendChild(text41);
+        let text51 = document.createElement("p");
+        text51.appendChild(e5);
+        text51.style.margin = "7px"
+        document.getElementById("Box51").appendChild(text51);
+        let text12 = document.createElement("p");
+        text12.appendChild(z1);
+        text12.style.margin = "7px"
+        document.getElementById("Box12").appendChild(text12);
+        let text22 = document.createElement("p");
+        text22.appendChild(z2);
+        text22.style.margin = "7px"
+        document.getElementById("Box22").appendChild(text22);
+        let text32 = document.createElement("p");
+        text32.appendChild(z3);
+        text32.style.margin = "7px"
+        document.getElementById("Box32").appendChild(text32);
+        let text42 = document.createElement("p");
+        text42.appendChild(z4);
+        text42.style.margin = "7px"
+        document.getElementById("Box42").appendChild(text42);
+        let text52 = document.createElement("p");
+        text52.appendChild(z5);
+        text52.style.margin = "7px"
+        document.getElementById("Box52").appendChild(text52);
+        let text13 = document.createElement("p");
+        text13.appendChild(d1);
+        text13.style.margin = "7px"
+        document.getElementById("Box13").appendChild(text13);
+        let text23 = document.createElement("p");
+        text23.appendChild(d2);
+        text23.style.margin = "7px"
+        document.getElementById("Box23").appendChild(text23);
+        let text33 = document.createElement("p");
+        text33.appendChild(d3);
+        text33.style.margin = "7px"
+        document.getElementById("Box33").appendChild(text33);
+        let text43 = document.createElement("p");
+        text43.appendChild(d4);
+        text43.style.margin = "7px"
+        document.getElementById("Box43").appendChild(text43);
+        let text53 = document.createElement("p");
+        text53.appendChild(d5);
+        text53.style.margin = "7px"
+        document.getElementById("Box53").appendChild(text53);
+        let text14 = document.createElement("p");
+        text14.appendChild(v1);
+        text14.style.margin = "7px"
+        document.getElementById("Box14").appendChild(text14);
+        let text24 = document.createElement("p");
+        text24.appendChild(v2);
+        text24.style.margin = "7px"
+        document.getElementById("Box24").appendChild(text24);
+        let text34 = document.createElement("p");
+        text34.appendChild(v3);
+        text34.style.margin = "7px"
+        document.getElementById("Box34").appendChild(text34);
+        let text44 = document.createElement("p");
+        text44.appendChild(v4);
+        text44.style.margin = "7px"
+        document.getElementById("Box44").appendChild(text44);
+        let text54 = document.createElement("p");
+        text54.appendChild(v5);
+        text54.style.margin = "7px"
+        document.getElementById("Box54").appendChild(text54);
+        let text15 = document.createElement("p");
+        text15.appendChild(f1);
+        text15.style.margin = "7px"
+        document.getElementById("Box15").appendChild(text15);
+        let text25 = document.createElement("p");
+        text25.appendChild(f2);
+        text25.style.margin = "7px"
+        document.getElementById("Box25").appendChild(text25);
+        let text35 = document.createElement("p");
+        text35.appendChild(f3);
+        text35.style.margin = "7px"
+        document.getElementById("Box35").appendChild(text35);
+        let text45 = document.createElement("p");
+        text45.appendChild(f4);
+        text45.style.margin = "7px"
+        document.getElementById("Box45").appendChild(text45);
+        let text55 = document.createElement("p");
+        text55.appendChild(f5);
+        text55.style.margin = "7px"
+        document.getElementById("Box55").appendChild(text55);
+        let body = document.getElementById("doppelt");
+        body.style.background = "linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(154,162,34,1) 12%, rgba(193,204,31,1) 43%, rgba(140,144,8,1) 67%, rgba(77,82,14,1) 100%)";
+    }
+}
---
+++ js/question.js
@@ -0,0 +1,200 @@
+function selectQuestion(questionId){
+    const Box = document.getElementById("Fragebox");
+    while (Box.hasChildNodes()){
+        Box.removeChild(Box.children[0]);
+    }
+    const Frage = document.createElement("p");
+    var Question;
+    var Done;
+    if(questionId=='q11'){
+        Question = document.createTextNode(config.Fragen.q11.Frage);
+        Done = document.getElementById("Box11");
+    }
+    if(questionId=='q12'){
+        Question = document.createTextNode(config.Fragen.q12.Frage);
+        Done = document.getElementById("Box12");
+    }
+    if(questionId=='q13'){
+        Question = document.createTextNode(config.Fragen.q13.Frage);
+        Done = document.getElementById("Box13");
+    }
+    if(questionId=='q14'){
+        Question = document.createTextNode(config.Fragen.q14.Frage);
+        Done = document.getElementById("Box14");
+    }
+    if(questionId=='q15'){
+        Question = document.createTextNode(config.Fragen.q15.Frage);
+        Done = document.getElementById("Box15");
+    }
+    if(questionId=='q21'){
+        Question = document.createTextNode(config.Fragen.q21.Frage);
+        Done = document.getElementById("Box21");
+    }
+    if(questionId=='q22'){
+        Question = document.createTextNode(config.Fragen.q22.Frage);
+        Done = document.getElementById("Box22");
+    }
+    if(questionId=='q23'){
+        Question = document.createTextNode(config.Fragen.q23.Frage);
+        Done = document.getElementById("Box23");
+    }
+    if(questionId=='q24'){
+        Question = document.createTextNode(config.Fragen.q24.Frage);
+        Done = document.getElementById("Box24");
+    }
+    if(questionId=='q25'){
+        Question = document.createTextNode(config.Fragen.q25.Frage);        
+        Done = document.getElementById("Box25");
+    }
+    if(questionId=='q31'){
+        Question = document.createTextNode(config.Fragen.q31.Frage);
+        Done = document.getElementById("Box31");
+    }
+    if(questionId=='q32'){
+        Question = document.createTextNode(config.Fragen.q32.Frage);
+        Done = document.getElementById("Box32");
+    }
+    if(questionId=='q33'){
+        Question = document.createTextNode(config.Fragen.q33.Frage);
+        Done = document.getElementById("Box33");
+    }
+    if(questionId=='q34'){
+        Question = document.createTextNode(config.Fragen.q34.Frage);
+        Done = document.getElementById("Box34");
+    }
+    if(questionId=='q35'){
+        Question = document.createTextNode(config.Fragen.q35.Frage);
+        Done = document.getElementById("Box35");
+    }
+    if(questionId=='q41'){
+        Question = document.createTextNode(config.Fragen.q41.Frage);
+        Done = document.getElementById("Box41");
+    }
+    if(questionId=='q42'){
+        Question = document.createTextNode(config.Fragen.q42.Frage);
+        Done = document.getElementById("Box42");
+    }
+    if(questionId=='q43'){
+        Question = document.createTextNode(config.Fragen.q43.Frage);
+        Done = document.getElementById("Box43");
+    }
+    if(questionId=='q44'){
+        Question = document.createTextNode(config.Fragen.q44.Frage);
+        Done = document.getElementById("Box44");
+    }
+    if(questionId=='q45'){
+        Question = document.createTextNode(config.Fragen.q45.Frage);
+        Done = document.getElementById("Box45");
+    }
+    if(questionId=='q51'){
+        Question = document.createTextNode(config.Fragen.q51.Frage);
+        Done = document.getElementById("Box51");
+    }
+    if(questionId=='q52'){
+        Question = document.createTextNode(config.Fragen.q52.Frage);
+        Done = document.getElementById("Box52");
+    }
+    if(questionId=='q53'){
+        Question = document.createTextNode(config.Fragen.q53.Frage);
+        Done = document.getElementById("Box53");
+    }
+    if(questionId=='q54'){
+        Question = document.createTextNode(config.Fragen.q54.Frage);
+        Done = document.getElementById("Box54");
+    }
+    if(questionId=='q55'){
+        Question = document.createTextNode(config.Fragen.q55.Frage);
+        Done = document.getElementById("Box55");
+    }
+    Done.style.visibility="hidden";
+    Frage.appendChild(Question);
+    Box.insertAdjacentElement("beforeend",Frage);
+    switchView("game-view");
+    current=questionId;
+}
+
+function ShowAnswer(){
+    const Box = document.getElementById("Fragebox");
+    while (Box.hasChildNodes()){
+        Box.removeChild(Box.children[0]);
+    }
+    var Answer;
+    if(current=='q11'){
+        Answer = document.createTextNode(config.Fragen.q11.Antwort);
+    }
+    if(current=='q12'){
+        Answer = document.createTextNode(config.Fragen.q12.Antwort);
+    }
+    if(current=='q13'){
+        Answer = document.createTextNode(config.Fragen.q13.Antwort);
+    }
+    if(current=='q14'){
+        Answer = document.createTextNode(config.Fragen.q14.Antwort);
+    }
+    if(current=='q15'){
+        Answer = document.createTextNode(config.Fragen.q15.Antwort);
+    }
+    if(current=='q21'){
+        Answer = document.createTextNode(config.Fragen.q21.Antwort);
+    }
+    if(current=='q22'){
+        Answer = document.createTextNode(config.Fragen.q22.Antwort);
+    }
+    if(current=='q23'){
+        Answer = document.createTextNode(config.Fragen.q23.Antwort);
+    }
+    if(current=='q24'){
+        Answer = document.createTextNode(config.Fragen.q24.Antwort);
+    }
+    if(current=='q25'){
+        Answer = document.createTextNode(config.Fragen.q25.Antwort);
+    }
+    if(current=='q31'){
+        Answer = document.createTextNode(config.Fragen.q31.Antwort);
+    }
+    if(current=='q32'){
+        Answer = document.createTextNode(config.Fragen.q32.Antwort);
+    }
+    if(current=='q33'){
+        Answer = document.createTextNode(config.Fragen.q33.Antwort);
+    }
+    if(current=='q34'){
+        Answer = document.createTextNode(config.Fragen.q34.Antwort);
+    }
+    if(current=='q35'){
+        Answer = document.createTextNode(config.Fragen.q35.Antwort);
+    }
+    if(current=='q41'){
+        Answer = document.createTextNode(config.Fragen.q41.Antwort);
+    }
+    if(current=='q42'){
+        Answer = document.createTextNode(config.Fragen.q42.Antwort);
+    }
+    if(current=='q43'){
+        Answer = document.createTextNode(config.Fragen.q43.Antwort);
+    }
+    if(current=='q44'){
+        Answer = document.createTextNode(config.Fragen.q44.Antwort);
+    }
+    if(current=='q45'){
+        Answer = document.createTextNode(config.Fragen.q45.Antwort);
+    }
+    if(current=='q51'){
+        Answer = document.createTextNode(config.Fragen.q51.Antwort);
+    }
+    if(current=='q52'){
+        Answer = document.createTextNode(config.Fragen.q52.Antwort);
+    }
+    if(current=='q53'){
+        Answer = document.createTextNode(config.Fragen.q53.Antwort);
+    }
+    if(current=='q54'){
+        Answer = document.createTextNode(config.Fragen.q54.Antwort);
+    }
+    if(current=='q55'){
+        Answer = document.createTextNode(config.Fragen.q55.Antwort);
+    }
+    const Frage = document.createElement("p")
+    Frage.appendChild(Answer);
+    Box.insertAdjacentElement("beforeend",Frage);
+}
